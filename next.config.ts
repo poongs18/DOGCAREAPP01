21f49b6e98255deed910d6175cf02f5164d29b07
poongs <poongs@thinktime.in>
2026-01-05 14:00:27 +0530

    force webpack build by disabling turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true
-// };
-
-// export default nextConfig;
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {
+  reactStrictMode: true
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
