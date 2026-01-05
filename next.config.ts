d4700f8a0a9902e0e47d2b9383b47045712b99b3
poongs <poongs@thinktime.in>
2026-01-05 13:21:35 +0530

    Disable turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
